bf6b4ef8779adf46518743a47bbf9b284d922370
Espen Nilsen <espen.k.nilsen@gmail.com>
2018-11-10 16:07:18 +0100

    noe oppdatering

--- UltraSonic.ino
+++ UltraSonic.ino
@@ -14,6 +14,17 @@ const int echoPin = 10;
 long duration;
 int distance;
 
+
+
+//distance in cm to define a car is in place (less than threshold)
+const int threshold = 200;
+
+
+
+
+
+
+
 void setup() {
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
     pinMode(echoPin, INPUT); // Sets the echoPin as an Input
@@ -31,8 +42,52 @@ void loop() {
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
     // Calculating the distance
-    distance= duration*0.034/2;
+    distance = (duration*0.034)/2;
     // Prints the distance on the Serial Monitor
     Serial.print("Distance: ");
     Serial.println(distance);
+
+
+    delay(500);
+    
 }
+
+
+void setThresholdDistance(int threshold) {
+   
+}
+
+
+boolean carParked() {
+  return (threshold < distance);
+}
+
+
+//Bedre navn
+void displayParked() {
+  //do something with some pins
+  //Blink lights
+  //sound the alarm
+
+  //start timer - Run for at least 10 seconds
+  //stop timer if car no longer parked
+
+
+ // while(carParked() && !timerExpired()){}
+  
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
