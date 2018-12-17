c4f4bf7a6956991a7c408768d1dfbbb06ce02af5
Espen Nilsen <espen.k.nilsen@gmail.com>
2018-12-17 10:53:50 +0100

    remove unused code

--- UltraSonic.ino
+++ UltraSonic.ino
@@ -44,12 +44,6 @@ void setup() {
         runtimeStorage.threshold = Support::defaultThreshold;
     }
 
-    if (runtimeStorage.alarmEnabeld != 0 && runtimeStorage.alarmEnabeld != 1) 
-    {
-        //runtimeStorage.alarmEnabeld  is not a boolean value
-        runtimeStorage.alarmEnabeld = Support::defaultAlarmSetting;
-    } 
-
     if (runtimeStorage.alarmEnabeld == false) 
     { 
         Serial.println(F("Alarm is disabled"));
@@ -69,17 +63,11 @@ void loop() {
     // put your main code here, to run repeatedly:
     control.iterateStatemachine();
 
-    //handleConsole();
-
-    //printMenu();
-
     runMenu(serialGetInt(false));
 
     delay(LOOPDELAY);
     
 }
-
-
 
 int thresholdHelper()
 {
@@ -163,61 +151,6 @@ void runMenu(int option)
 }
 
 
-
-
-/*
-///Function for manually test sub-systems
-int debugMode() 
-{
-
-    //Wait for input 
-    for (;;) 
-    {
-        if (Serial.available() > 0) 
-        {
-            break;
-        }
-    }
-
-    
-    // read the incoming string:
-    String data = Serial.readStringUntil("\n");
-
-    if (data == F("debug off") || data == F("exit")) 
-    {
-        //Exit debug mode
-        Serial.println(F("Exiting debug mode!"));
-        return 0;
-
-    } 
-    else if (data == F("alarm on")) 
-    {
-        buzzer.startAlarm();
-    } 
-    else if (data == F("alarm off")) 
-    {
-        buzzer.stopAlarm();
-    } 
-    else if (data == F("indicator on")) 
-    {
-        indicator.turnOn();
-    } 
-    else if (data == F("indicator off")) 
-    {
-        indicator.turnOff();
-    }
-    else 
-    {
-        Serial.println(F("Unrecognized command!"));
-        printDebugCmds();
-    }
-
-
-	return 1;
-
-}
-*/
-
 int serviceMenu()
 {
 
@@ -292,111 +225,6 @@ void plotPrint(bool on)
     }
     
 }
-
-
-
-
-
-
-/*
-void handleConsole() 
-{
-    
-    // reply only when you receive data:
-	if (Serial.available() > 0) 
-    {
-		// read the incoming string:
-        String data = Serial.readStringUntil("\n");
-
-        if (data == F("debug on")) 
-        {
-            Serial.println(F("Entering debug mode!"));
-            printDebugCmds();
-
-            //Hand over execution to debug mode
-            while(debugMode()){};
-            
-        } 
-        else if (data == F("print on")) 
-        {
-            sensor.setPrint(true);
-
-        } 
-        else if (data == F("print off")) 
-        {
-            sensor.setPrint(false);
-
-        } 
-        else if (data == F("plot on")) 
-        {
-            plotPrint(true);
-        }
-        else if (data == F("plot off")) 
-        {
-            plotPrint(false);
-        }
-        else if (data == F("threshold set")) 
-        {
-            
-            Serial.print(F("Enter threshold value in cm: "));
-            //String newThresholdString = Serial.readStringUntil("\n");
-            //int newThreshold =  atoi( newThresholdString.c_str() );
-            while (!Serial.available()) 
-            {
-                delay(20); //wait for input
-            }
-
-            int newThreshold = Serial.parseInt();
-            
-            runtimeStorage.threshold = newThreshold;
-
-            Serial.println();
-            Serial.print("Got value: ");
-            Serial.println(newThreshold, DEC);
-            Serial.println("To SAVE to eeprom run \"threshold store\"");
-            
-
-        } 
-        else if (data == F("threshold get")) 
-        {
-            Serial.print(F("Threshold value in cm: "));
-            Serial.println(runtimeStorage.threshold, DEC);
-            
-        } 
-        else if (data == F("threshold store"))
-        {
-            saveStorage(storage);
-            Serial.println(F("Stored to EEPROM"));
-
-        } 
-        else if (data == F("threshold eeprom")) 
-        {
-            Serial.print(F("Threshold stored in EEPROM is: "));
-            int t = storage.threshold;
-            Serial.println(t, DEC);
-            
-        } 
-        else if (data == F("threshold eeprom")) 
-        {
-            Serial.print(F("Threshold stored in EEPROM is: "));
-            int t = storage.threshold;
-            Serial.println(t, DEC);
-            
-        } 
-        else if (data == F("print config"))
-        {
-            serialPrintConfiguration(runtimeStorage, storage);
-        }
-        else 
-        {
-            Serial.println(F("Unrecognized command!"));
-            printRunCmds();
-        }
-    }
-}
-*/
-
-
 
 
 void ISRHandler() 
