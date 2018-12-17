cbf05941241bad47a9a9c6277188e142d4a6f49f
Espen Nilsen <espen.k.nilsen@gmail.com>
2018-12-17 09:40:04 +0100

    New menu system

--- serialsupport.h
+++ serialsupport.h
@@ -2,6 +2,7 @@
 #define BB_SERIAL_SUPPORT
 
 #include "Arduino.h"
+#include "menuEnums.h"
 
 void printDebugCmds() 
 {
@@ -15,6 +16,8 @@ void printDebugCmds()
     Serial.flush();
 
 }
+
+
 
 void printRunCmds() 
 {
@@ -31,6 +34,71 @@ void printRunCmds()
 }
 
 
+void printMenu()
+{
+    Serial.print(F("\n\n"));
+    Serial.println(F("Menu:"));
+
+    Serial.print(MENU_PRINT_ON, DEC);
+    Serial.println(F(": Print Mesurments - Enable"));
+
+    Serial.print(MENU_PRINT_OFF, DEC);
+    Serial.println(F(": Print Mesurments - Disable"));
+    
+    Serial.print(MENU_PLOT_ON, DEC);
+    Serial.println(F(": Plotting Mode - Enable"));
+
+    Serial.print(MENU_PLOT_OFF, DEC);
+    Serial.println(F(": Exit Plotting - Disable"));
+
+    Serial.print(MENU_THRESHOLD_SET, DEC);
+    Serial.println(F(": Threshold Set Runtime Value"));
+    
+    Serial.print(MENU_THRESHOLD_SET, DEC);
+    Serial.println(F(": Threshold Get Runtime Value"));
+
+    Serial.print(MENU_THRESHOLD_STORE, DEC);
+    Serial.println(F(": Threshold Store To EEPROM"));
+
+    Serial.print(MENU_THRESHOLD_EEPROM, DEC);
+    Serial.println(F(": Threshold Get Stored Value From EEPROM"));
+
+    Serial.print(MENU_PRINT_CONFIG, DEC);
+    Serial.println(F(": Print Configuration"));
+
+    Serial.print(MENU_ENTER_SERVICE, DEC);
+    Serial.println(F(": Enter Service Menu"));
+
+    
+    Serial.flush();
+}
+
+void printServiceMenu()
+{
+    Serial.print(F("\n\n"));
+    Serial.println(F("Service Menu:"));
+
+    Serial.print(MENU_SERVICE_EXIT, DEC);
+    Serial.println(F(": Exit Service Mode"));
+
+    Serial.print(MENU_SERVICE_ALARM_ON, DEC);
+    Serial.println(F(": Alarm On"));
+
+    Serial.print(MENU_SERVICE_ALARM_OFF, DEC);
+    Serial.println(F(": Alarm Off (Force)"));
+
+    Serial.print(MENU_SERVICE_INDICATOR_ON, DEC);
+    Serial.println(F(": Indicator On"));
+
+    Serial.print(MENU_SERVICE_INDICATOR_OFF, DEC);
+    Serial.println(F(": Indicator Off"));
+
+    Serial.print(MENU_SERVICE_CLEAR_EEPROM, DEC);
+    Serial.println(F(": Clear EEPROM"));
+
+    Serial.flush();
+}
+
 void serialPrintConfiguration(struct eepromStore runtimeStorage, struct eepromStore storage)
 {
     Serial.print(F("\n\n"));
@@ -41,7 +109,7 @@ void serialPrintConfiguration(struct eepromStore runtimeStorage, struct eepromSt
     Serial.print(runtimeStorage.threshold);
     Serial.println();
   
-    Serial.print(F("Alarm Enabeld:"));
+    Serial.print(F("Alarm Enabled:"));
     Serial.println(runtimeStorage.alarmEnabeld);
     
     Serial.println();
@@ -52,7 +120,7 @@ void serialPrintConfiguration(struct eepromStore runtimeStorage, struct eepromSt
     Serial.print(storage.threshold);
     Serial.println();
   
-    Serial.print(F("Alarm Enabeld:"));
+    Serial.print(F("Alarm Enabled:"));
     Serial.println(storage.alarmEnabeld);
     
     Serial.flush();
