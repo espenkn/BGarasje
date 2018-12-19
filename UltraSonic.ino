39bbc3848c820a7704e1e3d12485f21bd2165bb9
Espen Nilsen <espen.k.nilsen@gmail.com>
2018-12-19 19:23:27 +0100

    typos

--- UltraSonic.ino
+++ UltraSonic.ino
@@ -244,10 +244,10 @@ void plotPrint(bool on)
     } 
     else
     {
-        //1. silience all modules that print
+        //1. unsilience all modules that print
         indicator.setPrint(true);
         buzzer.setPrint(true);
-        //2. Enable mesurment serialPrintConfiguration
+        //2. disable mesurment serialPrintConfiguration
         sensor.setPrint(false);
     }
     
